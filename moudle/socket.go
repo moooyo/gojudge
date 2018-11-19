77b9faca1c6a9a2042d1cfcfcfebcf52ed5cca58
Maymomo <3466365807@qq.com>
2018-11-19 23:13:31 +0800

    modify socket.go

--- moudle/socket.go
+++ moudle/socket.go
@@ -1,32 +1,38 @@
 package moudle
 
+
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"io"
 	"net"
 	"unsafe"
 )
+
 var intTemp int
+
 const intSize int64=int64(unsafe.Sizeof((intTemp)))
-func socketRead(conn net.Conn,data []byte)(size int,err error) {
+
+func socketRead(conn net.Conn)(buf []byte,err error) {
 	sizeReader:=io.LimitReader(conn,intSize)
 	temp:=make([]byte,intSize)
-	_,err=sizeReader.Read(temp)
+	sizeReader.Read(temp)
+	fmt.Println(binary.LittleEndian.Uint64(temp))
 	if err!=nil{
-		return 0,err
+		return nil ,err
 	}
-	dataReader:=io.LimitReader(conn,int64(binary.BigEndian.Uint64(temp)))
-	var buf []byte
-	size,err=dataReader.Read(buf)
-	return size,err
+	dataReader:=io.LimitReader(conn,int64(binary.LittleEndian.Uint64(temp)))
+	buf = make([]byte, binary.LittleEndian.Uint64(temp))
+	_,err=dataReader.Read(buf)
+	return buf,err
 }
 
 func socketWrite(conn net.Conn,data []byte)(size int,err error){
-		size=len(data)
-		var buf=bytes.NewBuffer(make([]byte,0))
-		binary.Write(buf,binary.BigEndian,intSize)
-		binary.Write(buf,binary.BigEndian,data)
-		size,err=conn.Write(buf.Bytes())
-		return size,err
+	size=len(data)
+	var buf=bytes.NewBuffer(make([]byte,0))
+	binary.Write(buf,binary.LittleEndian, int64(size))
+	binary.Write(buf,binary.LittleEndian,data)
+	size,err=conn.Write(buf.Bytes())
+	return size,err
 }
