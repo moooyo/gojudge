39694c1365b9f3506b236dcbf189db0f9a83422c
Maymomo <3466365807@qq.com>
2018-12-07 22:54:01 +0800

    update test_client

--- test_client/main.go
+++ test_client/main.go
@@ -4,33 +4,28 @@ import (
 	"../def"
 	"../moudle"
 	"log"
-	"net"
 )
 
 func main() {
 
-	socket, err := moudle.Dial("127.0.0.1:8080")
-	go func(conn net.Conn) {
-		for i := 0; i < 10; i++ {
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			submit := def.Submit{
-				SubmitID:   i,
-				ProblemID:  1000,
-				CodeSource: []byte("#include <stdio.h> \n int main(void) { printf(\"1 2 3 4 5\"); return 0;}"),
-				Language:   def.CLanguage,
-			}
-			encoder := moudle.NewEnCoder()
-			encoder.AppendStruct(&submit)
-			encoder.Send(conn)
-
-			if err != nil {
-				log.Println(err)
-			}
-
-			conn.Close()
+	for i := 0; i < 100; i++ {
+		conn, err := moudle.Dial("127.0.0.1:8080")
+		if err != nil {
+			log.Fatal(err)
 		}
-	}(socket)
+		submit := def.Submit{
+			SubmitID:   i,
+			ProblemID:  1000,
+			CodeSource: []byte("#include <stdio.h> \n int main(void) { printf(\"1 2 3 4 5\"); return 0;}"),
+			Language:   def.CLanguage,
+		}
+		encoder := moudle.NewEnCoder()
+		encoder.AppendStruct(&submit)
+		encoder.Send(conn)
+
+		if err != nil {
+			log.Println(err)
+		}
+		conn.Close()
+	}
 }
